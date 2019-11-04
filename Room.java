0c9a673c2c88d1361b5d0e876c1ff2e27fff1891
JBrod918 <50709564+JBrod918@users.noreply.github.com>
2019-11-04 12:31:55 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -5,11 +5,20 @@ public class Room {
 	String building;
 	String number;
 	
+	Time[][] times = new Time[30][96];
+	
 	ArrayList<Reservation> events = new ArrayList<Reservation>();
 	
 	public Room(String name, String building, String number) {
 		this.name = name;
 		this.building = building;
 		this.number = number;
+	}
+	
+	boolean checkFree(int start, int end, int day) {
+		for(int i = start; i < end; i++) {
+			if(times[day][i].status == 2) return false;
+		}
+		return true;
 	}
 }
