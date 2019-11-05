acf991f6ffe0ad89324dccc1126cbf4dcbe12a8e
JBrod918 <50709564+JBrod918@users.noreply.github.com>
2019-11-05 11:00:12 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -16,8 +16,20 @@ public class Room {
 	}
 	
 	boolean checkFree(int start, int end, int day) {
+		//returns true if a reservation between the start and end points on the given day is completely free
 		for(int i = start; i < end; i++) {
 			if(times[day][i].status == 2) return false;
+		}
+		return true;
+	}
+	
+	boolean newRes(int start, int end, int day, int repeating, String name, String reserver) {
+		if(repeating == 0 && checkFree(start, end, day)) events.add(new Reservation(start, end, day, repeating, name, reserver));
+		else {
+			for(int i = 0; i < 30; i += repeating) if(!checkFree(start, end, day+i)) return false;
+			for(int i = 0; i < 30; i += repeating) {
+				events.add(new Reservation(start, end, day+i, repeating, name, reserver));
+			}
 		}
 		return true;
 	}
